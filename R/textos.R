cc000fa5de195401343bb16de85742d21330c2a7
Alejandro Cano <alejandrocanomn@gmail.com>
2022-12-18 15:52:24 -0500

    texto introductorio a grafico por grupo referencia

---
+++ R/textos.R
@@ -0,0 +1,26 @@
+### textos
+
+txt_info_m2 <- '
+                              <p align="justify">
+                              En este apartado, se presentan gráficas de dispersión que permiten analizar si existe algún tipo de relación entre los puntajes obtenidos por los estudiantes en las pruebas Saber 11 y Saber Pro. Estas gráficas se construyeron para cada grupo de referencia, teniendo en cuenta que se comparan pruebas similares del examen Saber 11 con Saber Pro, es decir, se comparó Lectura Crítica (Saber Pro) vs. Lectura Crítica (Saber 11), Razonamiento Cuantitativo (Saber Pro) vs. Matemáticas (Saber 11), Inglés (Saber Pro) vs. Inglés (Saber 11) y por supuesto, el puntaje global de ambos exámenes.
+                              En los gráficos de dispersión, el eje x corresponde a los puntajes del examen Saber 11, mientras que el eje y corresponde a los puntajes del examen Saber Pro. Se tiene una línea horizontal que corresponde al promedio nacional de la correspondiente prueba, en el grupo de referencia determinado. Por otro lado, la línea vertical, denota el promedio nacional, del correspondiente módulo.
+                              Con las líneas que denotan los promedios nacionales en las gráficas se conforman cuatro cuadrantes, así:
+                              </br></br>
+                              Cuadrante 1: superior derecho
+                              Cuadrante 2: superior izquierdo
+                              Cuadrante 3: inferior izquierdo
+                              Cuadrante 4: inferior derecho
+                              </br></br>
+                              En cada uno de estos cuadrantes, se muestra el porcentaje de estudiantes del correspondiente programa y prueba, que pertenece a dicho cuadrante, de manera que los porcentajes en cada cuadrante se leen así:
+                              </br></br>
+                              Cuadrante 1: estudiantes que obtuvieron puuntaje superior al promedio en Saber 11 y en Saber Pro
+                              Cuadrante 2: estudiantes que obtuvieron puntaje inferior al promedio en Saber 11, pero superior en Saber Pro
+                              Cuadrante 3: estudiantes que obtuvieron puntaje inferior al promedio en Saber 11 y en Saber Pro
+                              Cuadrante 4: estudiantes que obtuvieron puntaje superoir al promedio en saber 11, pero inferior en Saber Pro
+                              En los gráficos de dispersión, también se dividen los puntos en dos grupos (colores) que corresponden a dos periodos:
+                              </br></br>
+                              Periodo 1: puntajes de los estudiantes que presentaron el exmane Saber 11 antes del 2014-2
+                              Periodo 2: puntajes de los estudiantes que presentaron el examen Saber 11 después del 2014-2
+                              La anterior clasificación por periodos se realizó teniendo en cuenta por un lado que, el examen Saber Pro cambió en su estructura, agrupando algunas pruebas (mayor información); y por otro, algunas variaciones globales en los puntajes de ambos periodos.
+                              </br></br>
+                              </p>'
