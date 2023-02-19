84e501edb01086b1c1587c57dffbe154d82052aa
Alejandro Cano <alejandrocanomn@gmail.com>
2023-02-19 00:57:04 -0500

    dfs agregados como variables globales

--- R/globals.R
+++ R/globals.R
@@ -1,1 +1,1 @@
-utils::globalVariables(c("resultados", "mediasSaberPro", "mediasSaber11"))
+utils::globalVariables(c("resultados", "mediasSaberPro", "mediasSaber11","ATTdf","ATTprog","resultados_modelos","resultados_modelos_prog"))
