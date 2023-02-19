fb6deb715939b63bc5e4144b88db9661f2f40109
Alejandro Cano <alejandrocanomn@gmail.com>
2023-02-18 23:39:35 -0500

    data-raw datos a nivel de programa

---
+++ data-raw/ATTprog.R
@@ -0,0 +1,5 @@
+## code to prepare `ATTprog` dataset goes here
+
+load('data/ATTprog.rda')
+
+usethis::use_data(ATTprog, overwrite = TRUE)
---
+++ data-raw/resultados_modelos_prog.R
@@ -0,0 +1,5 @@
+## code to prepare `resultados_modelos_prog` dataset goes here
+
+load('data/resultados_modelos_prog.rda')
+
+usethis::use_data(resultados_modelos_prog, overwrite = TRUE)
